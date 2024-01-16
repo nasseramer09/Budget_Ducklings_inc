85132a772e63a96a1369e783c58f06224699a8d3
Nasser Amer <nasser_amer09@yahoo.com>
2024-01-16 22:36:32 +0100

    coded some codes

--- src/main/java/com/example/budget_ducklings_inc/repository/DataBaseConnector.java
+++ src/main/java/com/example/budget_ducklings_inc/repository/DataBaseConnector.java
@@ -8,26 +8,26 @@ public class DataBaseConnector {
 
     public DataBaseConnector(){
 
-        String Jdbc="com.mysql.jdbc.Driver";
         try {
-            Class.forName(Jdbc);
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Budget_Ducklings_inc");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select from emoplyees_table where UserName=? and PASSWORD=?");
 
-            while (resultSet.next()){
-               // int id=resultSet.getInt("id");
-                String UserName=resultSet.getString("UserName");
-                String password=resultSet.getString("PASSWORD");
-
-                System.out.println(UserName);
-                System.out.println(password);
-
-            }
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public boolean UserKontroll(String userName, String passWord) {
+        String sqlCommand="select from employees_table where UserName=? and PASSWORD=?";
 
+        try {
+
+                PreparedStatement statement = connection.prepareStatement(sqlCommand);
+                statement.setString(1, userName);
+                statement.setString(2, passWord);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
 }
