82770bc7d82e57ef917506317fd52f6d14d11390
Nasser Amer <nasser_amer09@yahoo.com>
2024-01-17 21:27:13 +0100

    created invoicePage and coded table where inoice components are going to beshown

--- src/main/java/com/example/budget_ducklings_inc/model/Components.java
+++ src/main/java/com/example/budget_ducklings_inc/model/Components.java
@@ -3,8 +3,14 @@ package com.example.budget_ducklings_inc.model;
 public class Components {
     private String userName;
     private String password;
-    private String invoiceTyp;
-    private double price;
+    private String title;
+    private String kategori;
+    private  String agare;
+    private String datum;
+    private String beskrivning;
+    private double pris;
+
+
     public String getUserName() {
         return userName;
     }
@@ -21,21 +27,51 @@ public class Components {
         this.password = password;
     }
 
-    public String getInvoiceTyp() {
-        return invoiceTyp;
+    public String getTitle() {
+        return title;
     }
 
-    public void setInvoiceTyp(String invoiceTyp) {
-        this.invoiceTyp = invoiceTyp;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public double getPrice() {
-        return price;
+    public double getPris() {
+        return pris;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setPris(double pris) {
+        this.pris = pris;
     }
 
+    public String getKategori() {
+        return kategori;
+    }
 
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getAgare() {
+        return agare;
+    }
+
+    public void setAgare(String agare) {
+        this.agare = agare;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getBeskrivning() {
+        return beskrivning;
+    }
+
+    public void setBeskrivning(String beskrivning) {
+        this.beskrivning = beskrivning;
+    }
 }
